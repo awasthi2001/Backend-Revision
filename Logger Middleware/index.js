4cefcf96ff583812aa3e51f61096822e695e57b2
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-30 14:18:21 +0530

    Logger Middleware using morgan package

---
+++ Logger Middleware/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var morgan = require('morgan')
+const app = express();
+
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'))
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the logger middleware!")
+})
+
+
+
+
+app.listen(3001,()=>{
+    console.log("running on 3001")
+})
