c0ddbf00de7d34e7db085df13541f95debd76b5e
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-31 07:42:33 +0530

    invalid route example

--- QueryAndParams/index.js
+++ QueryAndParams/index.js
@@ -12,13 +12,19 @@ app.get('/weather',(req,res)=>{
     const weather = data[city];
     res.send(`Its ${weather} weather in ${city}`)
 })
+app.get('/',(req,res)=>{
+    res.send("Its a home route")
+})
 
 //Example of Params
 app.get('/students/:id',(req,res)=>{
     let {id} = req.params
     res.send(`this is data of student with id ${id}`)
 })
-
+//Invalid route error
+app.get('*',(req,res)=>{
+    res.send("<h1>Invalid route</h1>")
+})
 app.listen(3000,()=>{
     console.log('listening on ',3000);
 })
