4ba921444010867a7dc1cf89a39cecee9e2e7df4
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-06 15:42:44 +0530

    done

--- MVC Todo/Routes/todo.routes.js
+++ MVC Todo/Routes/todo.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
-import { getAllTodos, handlePost } from "../Controllers/todo.controller.js";
+import { getAllTodos, handleDelete, handlePost, handleUpdate } from "../Controllers/todo.controller.js";
 
 export const TodoRouter = Router();
 
 TodoRouter.get('/',getAllTodos)
 TodoRouter.post('/create',handlePost)
+TodoRouter.delete('/:id',handleDelete)
+TodoRouter.patch('/:id',handleUpdate)
