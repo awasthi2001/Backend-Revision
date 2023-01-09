e8170366c43af05e0f7488625cfe28fe81d10305
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-09 18:45:25 +0530

    basic folder structure

---
+++ Books App/index.js
@@ -0,0 +1,15 @@
+import {express} from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
+
+
+
+
+
+
+app.listen(PORT,()=>{
+
+});
