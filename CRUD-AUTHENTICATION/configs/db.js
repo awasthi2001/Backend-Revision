3dc861b031c4abbdf29538e5d41d0b86cb011860
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-12 11:49:13 +0530

    jwt done

---
+++ CRUD-AUTHENTICATION/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+export function Connection(){
+ 
+    return new Promise(function(resolve, reject){
+         mongoose.connect("mongodb://127.0.0.1:27017/crud-auth",(err)=>{
+            if(err){
+              reject('Unable to connect to DB')
+            }else{
+               resolve("Connected to DB")
+            }
+         })
+    })
+}
---
+++ CRUD-AUTHENTICATION/models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    {
+        "Name": {required : true, type: String},
+        "Email": {required : true, type: String},
+        "Password": {required: true, type: String}
+    },{
+        versionKey : false,
+        timeStamps : true
+    }
+)
+
+export const UserModel = mongoose.model("users",UserSchema)
