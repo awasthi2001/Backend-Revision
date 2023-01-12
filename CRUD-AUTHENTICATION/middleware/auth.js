7ae64a35e2f761bf59cf7e869f76e5f2501d119c
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-12 11:51:25 +0530

    some updates

--- CRUD-AUTHENTICATION/middleware/auth.js
+++ CRUD-AUTHENTICATION/middleware/auth.js
@@ -1,18 +1,18 @@
 import { secretKey } from "../index.js";
-import jwt from 'jsonwebtoken'
-export default function Auth(req,res,next) {
-    let token  = req.headers.authorization
- try {
-    jwt.verify(token,secretKey,(err,decode)=>{
-        if(decode){
-            next(); 
-        }else{
-            res.status(400).send({
-                message : 'login first'
-            })
-        }
-    })
- } catch (error) {
-    console.log(error)
- }
+import jwt from "jsonwebtoken";
+export default function Auth(req, res, next) {
+  let token = req.headers.authorization;
+  try {
+    jwt.verify(token, secretKey, (err, decode) => {
+      if (decode) {
+        next();
+      } else {
+        res.status(400).send({
+          message: "login first",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
