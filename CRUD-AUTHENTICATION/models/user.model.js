302c66bf7163da465a9b7ef6f1a95f3fb19d7ab4
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-12 11:52:12 +0530

    some updates

--- CRUD-AUTHENTICATION/models/user.model.js
+++ CRUD-AUTHENTICATION/models/user.model.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema(
-    {
-        "Name": {required : true, type: String},
-        "Email": {required : true, type: String},
-        "Password": {required: true, type: String}
-    },{
-        versionKey : false,
-        timeStamps : true
-    }
-)
+  {
+    Name: { required: true, type: String },
+    Email: { required: true, type: String },
+    Password: { required: true, type: String },
+  },
+  {
+    versionKey: false,
+    timeStamps: true,
+  }
+);
 
-export const UserModel = mongoose.model("users",UserSchema)
+export const UserModel = mongoose.model("users", UserSchema);
